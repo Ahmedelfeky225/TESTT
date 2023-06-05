a13ce4457c4bda484919e669b97fe6353b8e4a0e
Ahmed <elfekyahmed337@gmail.com>
2023-06-05 14:43:25 +0200

    firstt

---
+++ app.py
@@ -0,0 +1,39 @@
+import joblib as joblib
+from flask import Flask, request, jsonify
+import sys
+
+sys.modules['sklearn.externals.joblib'] = joblib
+import flask_restful
+from flask_restful import Resource, Api
+
+import joblib
+import pandas as pd
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+
+api = Api(app)
+
+model = joblib.load(open('Coronav', 'rb'))
+
+
+@app.route('/')
+def home():
+    return 'Corona result api 😧'
+
+
+@app.route("/predictions", methods=['post'])
+def predictions():
+    results = request.json['results']
+    predictions = model.predict([results, ])
+    if predictions[0] == 0:
+        return 'The person is Negative'
+    else:
+        return 'The person is positive '
+    # return 'z' #jsonify(list(predictions))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
